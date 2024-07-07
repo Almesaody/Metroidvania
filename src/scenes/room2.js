dfebc9ca0cda4fc923183c71b4e64843c68fdb3a
Almesaody <120131207+Almesaody@users.noreply.github.com>
2024-07-07 15:10:17 +0300

    made progress

--- src/scenes/room2.js
+++ src/scenes/room2.js
@@ -1,9 +1,18 @@
-import {setBackgoundColor} from "./roomUtils.js";
+import {setBackgoundColor, setMapColliders} from "./roomUtils.js";
 
 export function room2(k, roomData) {
     setBackgoundColor(k, "#a2aed5");
 
     k.camScale(4);
-    k.camPos(170, 270);
+    k.camPos(170, 100);
     k.setGravity(1000);
+
+    const roomLayers = roomData.layers;
+    const map = k.add([k.pos(0, 0), k.sprite("room2")]);
+
+    console.log(previousSceneData);
+    const colliders = roomLayers[4].objects;
+    setMapColliders(k, map, colliders);
+
+    const player = k.add(makePlayer(k));
 }
