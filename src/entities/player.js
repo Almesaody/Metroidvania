070399df27452069cc362ebb507f6479f562d613
Almesaody <120131207+Almesaody@users.noreply.github.com>
2024-07-22 00:58:20 +0300

    started adding sounds

--- src/entities/player.js
+++ src/entities/player.js
@@ -148,6 +148,7 @@ export function makePlayer(k) {
                     }
 
                     state.set(statePropsEnum.playerHp, state.current().maxPlayerHP);
+                    k.play("boom");
                     this.play("explode");
                 });
 
